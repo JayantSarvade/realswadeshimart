a7771627ad21920d439cc4aa5d08a2c9e043e484
Suraj Mahamuni <68770481+suraj7007@users.noreply.github.com>
2021-08-11 16:32:50 +0530

    did some changes in header & footer. Added admin profile & dashboard template

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('detail/', views.detailview, name='detail'),
+    path('cart/', views.cart, name='cart'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('list/', views.listview, name='list'),
+    path('twocolview/', views.twocolview, name='2col'),
+    path('threecolview/', views.threecolview, name='3col'),
+    path('fourcolview/', views.fourcolview, name='4col'),
+    path('adminprofile/', views.adminprofile, name='adminprofile'),
+    path('admindash/', views.admindashboard, name='admindash'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -5,3 +5,39 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+
+def detailview(request):
+    return render(request, 'product.html')
+
+
+def listview(request):
+    return render(request, 'category-list.html')
+
+
+def twocolview(request):
+    return render(request, 'category-2cols.html')
+
+
+def threecolview(request):
+    return render(request, 'category-3cols.html')
+
+
+def fourcolview(request):
+    return render(request, 'category-4cols.html')
+
+
+def adminprofile(request):
+    return render(request, 'adminprofile.html')
+
+
+def admindashboard(request):
+    return render(request, 'admindash.html')
